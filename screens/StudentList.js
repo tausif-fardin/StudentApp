ed36e929608287a15b93209b7430e81cbe49c58b
Tausif Fardin <tausiffardin4@gmail.com>
2023-01-07 20:40:47 +0600

    update

--- screens/StudentList.js
+++ screens/StudentList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import studentData from '../assets/MOCK_DATA.json';
-
 import {
   View,
   Text,
@@ -9,71 +8,83 @@ import {
   StyleSheet,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { ActivityIndicator } from 'react-native-paper';
+
+const DATA = studentData;
 
 const StudentList = () => {
   const navigation = useNavigation();
+  const [students, setStudents] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState([]);
-  const [page, setPage] = useState(1);
-
-  getUsers = (pageNumber) => {
-    const startIndex = (pageNumber - 1) * 10;
-    const endIndex = startIndex + 10;
-    const pageData = studentData.slice(startIndex, endIndex);
-    setUsers(pageData);
-    //console.log(pageData);
-    setLoading(false);
-  };
-
-  loadMore = () => {
-    setPage(page + 1);
-    setLoading(true);
-  };
+  const [hasMorePages, setHasMorePages] = useState(true);
 
   useEffect(() => {
-    getUsers(page);
-  }, [page]);
+    // Load the first page of students on component mount
+    loadMore();
+  }, []);
 
-  return (
-    <View style={{ padding: 20 }}>
-      {loading ? (
-        <Text>Loading...</Text>
-      ) : (
-        <FlatList
-          data={users}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate('StudentDetails', {
-                  studentId: item.id,
-                  studentImage: item.picture,
-                  studentFName: item.first_name,
-                  studentLName: item.last_name,
-                  studentEmail: item.email,
-                  studentGender: item.gender,
-                  studentClass: item.class,
-                })
-              }
-            >
-              <View style={styles.itemContainer}>
-                <Text style={styles.nameText}>
-                  {item.id}-{item.first_name}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )}
-          keyExtractor={({ id }) => id.toString()}
-          onEndReached={loadMore}
-          onEndReachedThreshold={0.5}
-        />
-      )}
+  const loadMore = () => {
+    if (loading) return;
+
+    setLoading(true);
+    setTimeout(function () {
+      // Load the next page of students
+      const nextStudents = DATA.slice(students.length, students.length + 10);
+      if (nextStudents.length === 0) {
+        // There are no more pages to load
+        setHasMorePages(false);
+      } else {
+        setStudents(students.concat(nextStudents));
+      }
+      setLoading(false);
+    }, 2000); //set timeout added so that the loader icon can show up.
+  };
+
+  const renderItem = ({ item }) => (
+    <View
+      style={{
+        flexDirection: 'row',
+        padding: 15,
+        marginVertical: 8,
+        marginHorizontal: 7,
+        backgroundColor: '#c4c4c4',
+      }}
+    >
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('StudentDetails', {
+            studentId: item.id,
+            studentImage: item.picture,
+            studentFName: item.first_name,
+            studentLName: item.last_name,
+            studentEmail: item.email,
+            studentGender: item.gender,
+            studentClass: item.class,
+          })
+        }
+      >
+        <View style={styles.itemContainer}>
+          <Text style={styles.nameText}>
+            {item.id}-{item.first_name}
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
+
+  return (
+    <FlatList
+      data={students}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      onEndReached={hasMorePages ? loadMore : null}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={loading && ActivityIndicator}
+    />
+  );
 };
+
 const styles = StyleSheet.create({
-  flatList: {
-    backgroundColor: '#eee',
-  },
   itemContainer: {
     padding: 16,
     borderBottomWidth: 1,
@@ -84,4 +95,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+
 export default StudentList;
